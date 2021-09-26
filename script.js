8abf435f07b0f00111c71fcf8b02ffe457960565
Beatriz Pontes <biapontes101@gmail.com>
2021-09-25 21:55:10 -0300

    alteracao fundo

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show");
 });
+
+const slides = document.querySelectorAll(".slide");
+const carrosel = document.getElementsById("carrosel");
+const left = document.getElementById("left");
+const rigth = document.getElementById("right");
